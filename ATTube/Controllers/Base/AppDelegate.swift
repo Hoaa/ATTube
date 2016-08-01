c82d09d20ff99a20bf3182624b32bce2be19fa40
Asiantech1 <asiantech1@AsianTechs-Mac-mini-4.local>
2016-08-01 13:56:36 +0700

    Completely create-project

---
+++ ATTube/Controllers/Base/AppDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  AppDelegate.swift
+//  ATTube
+//
+//  Created by Asiantech1 on 8/1/16.
+//  Copyright © 2016 at. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	static let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+	func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+		window = UIWindow(frame: UIScreen.mainScreen().bounds)
+		if let window = window {
+			self.window?.rootViewController = BaseTabBarController()
+			window.backgroundColor = UIColor.whiteColor()
+			window.makeKeyAndVisible()
+		}
+		return true
+	}
+
+	func applicationWillResignActive(application: UIApplication) {
+
+	}
+
+	func applicationDidEnterBackground(application: UIApplication) {
+
+	}
+
+	func applicationWillEnterForeground(application: UIApplication) {
+
+	}
+
+	func applicationDidBecomeActive(application: UIApplication) {
+
+	}
+
+	func applicationWillTerminate(application: UIApplication) {
+
+	}
+
+}
+
